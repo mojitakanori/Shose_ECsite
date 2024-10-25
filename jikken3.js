d19aa6d89a06efd54067e33469569c5825f333c4
TakanoriMoji <t.moji@mates-promo.com>
2024-10-25 14:40:43 +0900

    価格の上限と下限を指定できるようにした

--- jikken3.js
+++ jikken3.js
@@ -6,7 +6,6 @@ window.onload = () => {
 // 主な使用関数
 // document.getElementById('') -> html内にある要素をidによって取得
 
-
 // Searchボタンのハンドラ
 const onClickSearchHandler = () => {
     // アイテム表示欄を取得し空にする
@@ -19,11 +18,12 @@ const onClickSearchHandler = () => {
     // チェックボックスで選択されているジャンルを取得
     let selectedGenres = getSelectedGenres()
 
-    console.log(selectedGenres)
-    console.log("ここ！！！！！！！！selectedGenres  チェックボックスで選択されているジャンルを取得")
+    // 下限価格と上限価格を取得
+    const minPrice = document.getElementById('min-price').value;
+    const maxPrice = document.getElementById('max-price').value;
 
     // 検索クエリの作成と検索結果の反映
-    search(brand = text, genres = selectedGenres)
+    search(text, selectedGenres, minPrice, maxPrice)
 }
 
 let DATA = null;
@@ -31,117 +31,91 @@ let DATA = null;
 // 並び替えボタンのハンドラ
 const onChangeSortHandler = () => {
     // DATAをitemsに格納
-    const items = DATA
+    const items = DATA;
     // 並び替えるインデックスを取得
-    const orderIndx = document.getElementById('order').selectedIndex
+    const orderIndx = document.getElementById('order').selectedIndex;
 
-    // *************************
     // 並び替えに関する条件を追加
-    if (orderIndx == 0) {
-        // 取得順
-    } else if (orderIndx == 1) {
-        // 人気順
-        console.log("人気順にする！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！")
-        items.sort(function (a, b){
-            return b.num_of_favorite - a.num_of_favorite
-        });
-    } else if (orderIndx == 2) {
-        // ○○順
-    } else if (orderIndx == 3) {
-        // ××順
+    if (orderIndx == 1) {  // 人気順
+        items.sort((a, b) => b.num_of_favorite - a.num_of_favorite);
     }
-    // アイテム表示欄を取得し空にする,各アイテムを格納
-    const parent = document.getElementById('table-wrapper')
-        parent.innerHTML = ''
-        items.forEach(item => {
-                parent.appendChild(createItem(item))
-            })
-    // *************************
+
+    // アイテム表示欄を取得し空にする, 各アイテムを格納
+    const parent = document.getElementById('table-wrapper');
+    parent.innerHTML = '';
+    items.forEach(item => {
+        parent.appendChild(createItem(item));
+    });
 }
 
 // 検索クエリの作成と検索結果の反映
-const search = (brand = '', genres = []) => {
+const search = (brand = '', genres = [], minPrice = 0, maxPrice = 100000) => {
     // 引数の型チェック
-    if (typeof (brand) != 'string' || typeof (genres) != 'object') {
-        return "invalid argument"
+    if (typeof (brand) != 'string' || !Array.isArray(genres)) {
+        return "invalid argument";
     }
 
-    isLoading(true)
-    let queryStr = ''
-    let EAN = {}  // ExpressionAttributeNames
-    let EAV = {}  // ExpressionAttributeValues
+    isLoading(true);
+    let queryStr = '';
+    let EAN = {};  // ExpressionAttributeNames
+    let EAV = {};  // ExpressionAttributeValues
 
     // ブランドに関する条件を追加
-    if (brand != '') {
-        queryStr += '(#bra=:bra)'
-        EAN['#bra'] = 'brand'
-        EAV[':bra'] = brand
+    if (brand !== '') {
+        queryStr += '(#bra = :bra)';
+        EAN['#bra'] = 'brand';
+        EAV[':bra'] = brand;
     }
-    // *************************
-    // ジャンルに関する条件を追加 *
-    // *************************
-    if (genres.length != 0) {
-        if (brand != '') {
-            queryStr += 'AND'
-        }
-        if (genres.length != 1) {
-            queryStr += '('
-        }
-        EAN['#gen'] = 'genre'
-        for (let i = 0; i < genres.length; i++) {
-            if (i != 0) {
-                queryStr += 'OR'
-            }
-            queryStr += '(#gen=' + ':gen' + String(i) + ')'
-            EAV[':gen' + String(i)] = genres[i]
-        }
-        if (genres.length != 1) {
-            queryStr += ')'
-        }
-    }
-// ((#gen=:gen0)*****(#gen=:gen1)*****(#gen=:gen2)*****(#gen=:gen3))
-// "(#gen=:gen0)"
 
-    // 最終的なparamsを作る
-    let params
-    if (queryStr != '') {
-        params = {
-            TableName: tableName,
-            FilterExpression: queryStr,
-            ExpressionAttributeNames: EAN,
-            ExpressionAttributeValues: EAV
+    // ジャンルに関する条件を追加
+    if (genres.length > 0) {
+        if (queryStr !== '') {
+            queryStr += ' AND ';
         }
-    } else {
-        // 検索条件が何もないとき
-        params = { TableName: tableName }
+        queryStr += '(' + genres.map((_, i) => `#gen = :gen${i}`).join(' OR ') + ')';
+        EAN['#gen'] = 'genre';
+        genres.forEach((genre, i) => {
+            EAV[`:gen${i}`] = genre;
+        });
     }
-    console.log(params)
-    console.log("ここ！！！！！！！！params")
+
+    // 価格範囲に関する条件を追加
+    if (minPrice !== '0' || maxPrice !== '100000') {
+        if (queryStr !== '') {
+            queryStr += ' AND ';
+        }
+        queryStr += '(#price BETWEEN :minPrice AND :maxPrice)';
+        EAN['#price'] = 'price';
+        EAV[':minPrice'] = Number(minPrice);
+        EAV[':maxPrice'] = Number(maxPrice);
+    }
+
+    // クエリパラメータの設定
+    const params = queryStr ? {
+        TableName: tableName,
+        FilterExpression: queryStr,
+        ExpressionAttributeNames: EAN,
+        ExpressionAttributeValues: EAV
+    } : { TableName: tableName };
+
+    console.log(params);
 
     // 検索クエリを投げる
     docClient.scan(params, (err, data) => {
         if (err) {
-            // エラーのときの処理
-            console.log(err)
-            let errStr = '<span style="color:red;">' + 'ERROR<br>' + err + '</span>'
-            document.getElementById('table-wrapper').innerHTML = errStr
+            console.log(err);
+            const errStr = `<span style="color:red;">ERROR<br>${err}</span>`;
+            document.getElementById('table-wrapper').innerHTML = errStr;
         } else {
-            // 成功したときの処理
-            console.log(data)
-            console.log("ここ！！！！！！！！data返ってきたやつそのまま")
-            console.log(data.Items)
-            console.log("ここ！！！！！！！！data.Items")
-
-            DATA = data.Items
-            const parent = document.getElementById('table-wrapper')
+            console.log("データ取得成功:", data.Items);
+            DATA = data.Items;
+            const parent = document.getElementById('table-wrapper');
             data.Items.forEach(item => {
-                parent.appendChild(createItem(item))
-            })
+                parent.appendChild(createItem(item));
+            });
+            document.getElementById('item-num').innerText = `${data.Count}件Hit!!!`;
         }
-        // 検索結果の件数表示
-        const item_num = document.getElementById('item-num')
-        item_num.innerHTML = data.Count + '件Hit!!!'
-        isLoading(false)
-        onChangeSortHandler()
-    })
+        isLoading(false);
+        onChangeSortHandler();
+    });
 }
